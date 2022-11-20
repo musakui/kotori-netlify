e8f6c91bd53dffbe1e22c8f3c46ca3267994ee75
Shun Git <kwokshungit@gmail.com>
2022-11-20 23:17:05 +0800

    add icon

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,26 @@ import { defineConfig } from 'vite'
 import kotori from '@musakui/kotori/plugin'
 import { toPEM, fromPrivate } from '@musakui/fedi/keys'
 
-const publicKey = fromPrivate(toPEM(process.env.AP_PRIVATE_KEY))
+const url = process.env.URL
+
+const admin = {
+	isBot: true,
+	manuallyApprovesFollowers: true,
+	url,
+	name: 'kotori on Netlify',
+	summary: 'minimal ActivityPub instance. https://github.com/musakui/kotori-netlify',
+	icon: { type: 'Image', mediaType: 'image/png', url: `${url}/icon.png` },
+	inbox: '/inbox',
+	published: new Date(),
+	publicKey: fromPrivate(toPEM(process.env.AP_PRIVATE_KEY)),
+}
 
 export default defineConfig({
 	plugins: [
 		kotori({
-			domain: process.env.URL,
+			domain: url,
 			profiles: {
-				admin: {
-					isBot: true,
-					manuallyApprovesFollowers: true,
-					url: 'https://github.com/musakui/kotori-netlify',
-					published: new Date(),
-					inbox: '/inbox',
-					publicKey,
-				},
+				admin,
 			},
 			processHeaders: (h) => ({
 				_headers: Object.entries(h).map(([fn, hs]) => `/${fn}\n${
